b146441e4dad327dc787103c2dfdfd8530db3660
Alok <b22ci004@iitj.ac.in>
2025-11-09 20:10:59 +0530

    done !!!

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Vansh's Website 
+
+<img src="/src/assets/Screenshot 2025-07-28 at 12.23.09 PM.png" width="100%">
---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+
+import { EvervaultCard } from "./ui/evervault-card"
+
+export function Hero() {
+  return (
+    <section className="flex items-start mx-auto p-4 relative w-screen h-screen">
+
+      <EvervaultCard  />
+
+    </section>
+  );
+}
